95c6d25ad3c242944327754a9dd9ad2353e81da6
Pathfinder1613 <quin.strickland@outlook.com>
2024-12-24 19:58:35 -0600

    idk
    idk

--- SkyStone/GameLogic/renderCrads.js
+++ SkyStone/GameLogic/renderCrads.js
@@ -1,21 +1,19 @@
+
 import { addCardSelectionListener } from './gameBoard.js';
 
-export function renderStones(game, currentPlayerTurn) {
+export function renderStones(game) {
     const PlayerHand = document.getElementById('Player-Hand');
     PlayerHand.innerHTML = '';
 
-    const isGameBoardPage = window.location.pathname.includes('game-Board.html');
-
-    game.players.forEach((player, index) => {
-        if (isGameBoardPage && index !== currentPlayerTurn) return; // Skip rendering if it's not the current player's turn on game-Board.html
+       const currentPlayer = game.players[game.currentPlayer];
 
         const playerDiv = document.createElement("div");
         playerDiv.classList.add("player");
         const playerName = document.createElement("h2");
-        playerName.textContent = `${player.name}'s Skystones`;
+        playerName.textContent = `${currentPlayer.name}'s Skystones`;
         playerDiv.appendChild(playerName);
 
-        player.stones.forEach((stone) => {
+        currentPlayer.stones.forEach((stone) => {
             const card = document.createElement("div");
             card.classList.add("card");
 
@@ -36,7 +34,8 @@ export function renderStones(game, currentPlayerTurn) {
         });
 
         PlayerHand.appendChild(playerDiv);
-    });
 
-    addCardSelectionListener();
-}
+        addCardSelectionListener()
+    };
+
+ 
